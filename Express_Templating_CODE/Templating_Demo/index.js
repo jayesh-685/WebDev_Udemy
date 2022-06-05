4808b843b776d4a735c7e52b39837dcb54ca82d6
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-06-05 23:50:43 +0530

    express

--- Express_Templating_CODE/Templating_Demo/index.js
+++ Express_Templating_CODE/Templating_Demo/index.js
@@ -3,14 +3,23 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json');
 
+// by default, when we create a new express app and we're using some
+// view, Engine Express is going to assume that our views are templates exist in a directory called views.
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
+// __dirname refers to where index.js is located
 app.set('views', path.join(__dirname, '/views'))
+// the default views directory that Express is looking for is only going to work if I am running my application from within the same directory where my views folder is.
+// it uses process.cwd() and then appends /views to it to find views directory which doens't always work.
+// we want where my file index.js is  located and then append views to that
+// to achieve that we use const path and app.set('views', path.join())
 
 app.get('/', (req, res) => {
     res.render('home')
 })
+// dont need to write home.ejs because when we set view engine to ejs it assumes we are going to use ejs files
 
 app.get('/cats', (req, res) => {
     const cats = [
