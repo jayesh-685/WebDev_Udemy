018207660f4584a6a0d63e1c65b54d70859f2006
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-06-14 00:01:06 +0530

    mongoose

--- mongoosePractice/index.js
+++ mongoosePractice/index.js
@@ -7,3 +7,28 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
         console.log("OH NO ERROR!!!!")
         console.log(err)
     })
+
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+])
+.then(data => {
+    console.log("IT WORKED!")
+    console.log(data);
+})
+
+
+// for finding we can use Movie.find({}) which returns a query object which has tons of attributes but since it's a thennable obect we use Movie.fin({}).then(data => console.log(data)) to show all the data
